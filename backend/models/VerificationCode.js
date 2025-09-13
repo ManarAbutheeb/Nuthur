a5e7cd058efb342b23bd3e4e62db242fb62e3bde
Manar Abutheeb <manar.moab@gmail.com>
2025-09-13 21:22:41 +0300

    link login & signin & reset password with backend

---
+++ backend/models/VerificationCode.js
@@ -0,0 +1,23 @@
+// models/VerificationCode.js
+const mongoose = require('mongoose');
+
+const verificationCodeSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  expiresAt: {
+    type: Date,
+    default: Date.now,
+    expires: 600, //  الرمز ينحذف تلقائياً من قاعدة البيانات بعد 10 دقائق (600 ثانية)
+  },
+});
+
+// تأكد من أن MongoDB ستنشئ index لحذف المستندات المنتهية
+verificationCodeSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+module.exports = mongoose.model('VerificationCode', verificationCodeSchema);

7aea6fa22efb50d12e226f5002c6f2d983eb5d47
Renad293 <148154454+Renad293@users.noreply.github.com>
2025-11-11 20:19:57 +0300

    testing link

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,19 +1,15 @@
 "use client";
 
-// تأكدي أن كل المكونات موجودة بالمسار الصحيح
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import WeatherWidget from "../components/WeatherWidget";
-
 export default function HomePage() {
-  return (
-    <main>
-      {/* أي مكون يحتاج client */}
-      <WeatherWidget />
 
-      {/* المكونات الرئيسية */}
+  return (
+    <>
+    <WeatherWidget />
       <Hero />
       <Features />
-    </main>
+    </>
   );
 }

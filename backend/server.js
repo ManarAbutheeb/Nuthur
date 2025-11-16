a2d4af96a740f803b482aa30c1d92fe3cdaff1a3
Manar Abutheeb <manar.moab@gmail.com>
2025-11-16 16:43:16 +0300

    add in server

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({ 
-  origin: ["http://localhost:3000", "https://nuthur.up.railway.app"], 
+  origin: ["http://localhost:3000", "https://nuthur.up.railway.app","https://nuthur-production.up.railway.app"], 
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],

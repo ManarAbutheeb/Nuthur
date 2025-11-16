ad5355a2883c4f019b36c20c93d490e276806265
Manar Abutheeb <manar.moab@gmail.com>
2025-11-16 16:59:43 +0300

    moving corn up in server

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,15 @@ const scheduledCheckRoutes = require("./routes/scheduledCheckRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
-app.use(express.json());
-
 app.use(cors({ 
   origin: ["http://localhost:3000", "https://nuthur.up.railway.app","https://nuthur-production.up.railway.app"], 
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 }));
+app.use(express.json());
+
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))

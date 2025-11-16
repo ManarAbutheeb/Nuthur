9adcd01af7a8f7c1c3195ad8eb805167234ccddf
Manar Abutheeb <manar.moab@gmail.com>
2025-11-16 17:31:44 +0300

    disabling cron job temporary

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cron = require("node-cron");
-const cors = require('cors'); 
-const ScheduledCheck = require("./models/ScheduledCheck");
+// const cors = require('cors'); 
+// const ScheduledCheck = require("./models/ScheduledCheck");
 const { generateWeatherData } = require("./services/weatherService");
-const { runModelPrediction } = require("./controllers/predictModel");
+// const { runModelPrediction } = require("./controllers/predictModel");
 require("dotenv").config();
-const User = require("./models/user");
-const nodemailer = require("nodemailer"); 
+// const User = require("./models/user");
+// const nodemailer = require("nodemailer"); 
 
 const authRoutes = require("./routes/authRoutes");
 const reportRoutes = require("./routes/reportRoutes");
@@ -61,62 +61,62 @@ const transporter = nodemailer.createTransport({
 });
 
 
-cron.schedule("0 */8 * * *", async () => {
-// cron.schedule("* * * * *", async () => {
+// cron.schedule("0 */8 * * *", async () => {
+// // cron.schedule("* * * * *", async () => {
 
-  console.log(" Running automatic weather check...");
-  try {
-    const lat = 18.2353699; // السودة
-    const lng = 42.3895230;
+//   console.log(" Running automatic weather check...");
+//   try {
+//     const lat = 18.2353699; // السودة
+//     const lng = 42.3895230;
 
-    const weatherRecord = await generateWeatherData(lat, lng, null);
-    const now = new Date();
-    const prediction = await runModelPrediction({
-      day: now.getDate(),
-      month: now.getMonth() + 1,
-      year: now.getFullYear(),
-      Temperature: weatherRecord.temperature,
-      RH: weatherRecord.humidity,
-      Ws: weatherRecord.windSpeed,
-      Rain: weatherRecord.rainfall,
-      FFMC: weatherRecord.indices.ffmc,
-      DMC: weatherRecord.indices.dmc,
-      DC: weatherRecord.indices.dc,
-      ISI: weatherRecord.indices.isi,
-      BUI: weatherRecord.indices.bui,
-      FWI: weatherRecord.indices.fwi,
-    });
+//     const weatherRecord = await generateWeatherData(lat, lng, null);
+//     const now = new Date();
+//     const prediction = await runModelPrediction({
+//       day: now.getDate(),
+//       month: now.getMonth() + 1,
+//       year: now.getFullYear(),
+//       Temperature: weatherRecord.temperature,
+//       RH: weatherRecord.humidity,
+//       Ws: weatherRecord.windSpeed,
+//       Rain: weatherRecord.rainfall,
+//       FFMC: weatherRecord.indices.ffmc,
+//       DMC: weatherRecord.indices.dmc,
+//       DC: weatherRecord.indices.dc,
+//       ISI: weatherRecord.indices.isi,
+//       BUI: weatherRecord.indices.bui,
+//       FWI: weatherRecord.indices.fwi,
+//     });
 
-    const check = new ScheduledCheck({
-      location: { lat, lng },
-      weatherData: weatherRecord._id,
-      modelPrediction: prediction.prediction === 1 ? "High Risk" : "No Risk",
-      modelCheckedAt: new Date()
-    });
+//     const check = new ScheduledCheck({
+//       location: { lat, lng },
+//       weatherData: weatherRecord._id,
+//       modelPrediction: prediction.prediction === 1 ? "High Risk" : "No Risk",
+//       modelCheckedAt: new Date()
+//     });
 
-    await check.save();
-    console.log(" Scheduled check saved:", check.modelPrediction);
+//     await check.save();
+//     console.log(" Scheduled check saved:", check.modelPrediction);
 
    
-    if (check.modelPrediction === "High Risk") {
-      const volunteers = await User.find({ role: "volunteer", isVerified: true });
+//     if (check.modelPrediction === "High Risk") {
+//       const volunteers = await User.find({ role: "volunteer", isVerified: true });
 
-      for (const vol of volunteers) {
-        await transporter.sendMail({
-          from: `"Nuthur Support" <${process.env.EMAIL_USER}>`,
-          to: vol.email,
-          subject: " تنبيه: خطر حريق في منطقة السودة",
-          html: `<p>مرحباً ${vol.name},</p>
-                 <p>تنبيه! نموذج النظام توقع وجود خطر حريق عالي في منطقة السودة حالياً.</p>
-                 <p>يرجى أخذ الحيطة والحذر.</p>`,
-        });
-      }
-    }
+//       for (const vol of volunteers) {
+//         await transporter.sendMail({
+//           from: `"Nuthur Support" <${process.env.EMAIL_USER}>`,
+//           to: vol.email,
+//           subject: " تنبيه: خطر حريق في منطقة السودة",
+//           html: `<p>مرحباً ${vol.name},</p>
+//                  <p>تنبيه! نموذج النظام توقع وجود خطر حريق عالي في منطقة السودة حالياً.</p>
+//                  <p>يرجى أخذ الحيطة والحذر.</p>`,
+//         });
+//       }
+//     }
 
-  } catch (err) {
-    console.error(" Scheduled check failed:", err.message);
-  }
-});
+//   } catch (err) {
+//     console.error(" Scheduled check failed:", err.message);
+//   }
+// });
 
 
 app.use("/api/auth", authRoutes);

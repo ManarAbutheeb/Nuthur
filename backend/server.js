e1079279ea34cab5e1e3488a497ce8dc5ddeb0fa
Manar Abutheeb <manar.moab@gmail.com>
2025-11-16 04:22:00 +0300

    fix: login work

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({ 
-  origin: process.env.FRONTEND_URL, 
-  credentials: true 
+  origin: ["http://localhost:3000", "https://nuthur.up.railway.app"], 
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 mongoose.connect(process.env.MONGO_URI)
